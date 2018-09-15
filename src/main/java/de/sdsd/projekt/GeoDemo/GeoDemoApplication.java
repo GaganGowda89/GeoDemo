dbf76966b5b80c0e4f01e4d22c58c026081cec29
Raj Shah <raj.shah@seven-re.com>
2018-09-15 17:56:43 +0200

    Add spring boot

---
+++ src/main/java/de/sdsd/projekt/GeoDemo/GeoDemoApplication.java
@@ -0,0 +1,12 @@
+package de.sdsd.projekt.GeoDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoDemoApplication.class, args);
+	}
+}
